d99c2570ea1800219bc7646562fde234374f9da0
TaniaTru <tatiana.trukhachova@gmail.com>
2018-02-06 20:13:46 +0200

    Registered dependency

--- MyStore.WebUI/App_Start/NinjectWebCommon.cs
+++ MyStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,7 @@ namespace MyStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new MyStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
