c25e2550a4782a5af29c42aa3de8d1e3614afa22
TaniaTru <tatiana.trukhachova@gmail.com>
2018-02-20 13:24:48 +0200

    Updated List() in ProductController Updated PaginationTests

--- MyStore.WebUI/Controllers/ProductController.cs
+++ MyStore.WebUI/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyStore.WebUI.Models;
 
 namespace MyStore.WebUI.Controllers
 {
@@ -17,10 +18,19 @@ namespace MyStore.WebUI.Controllers
         }
         public ViewResult List(int page =1)
         {
-            return View(repository.Products
-                .OrderBy(p => p.ProductID)
+            ProductsListViewModel model = new ProductsListViewModel {
+                Products = repository.Products
+                .OrderBy(p=>p.ProductID)
                 .Skip((page-1)*PageSize)
-                .Take(PageSize));
+                .Take(PageSize),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
+ 
         }
     }
 }
